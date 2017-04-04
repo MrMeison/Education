f98a9fb531020070084d68507b7344433c48c78e
Konstantin Mamaev <konstantin.mamaev@akvelon.com>
2017-04-04 20:37:14 +0300

    Add init for translator

--- skyeng/translator/translator.js
+++ skyeng/translator/translator.js
@@ -1,50 +1,104 @@
 ;
 (function (document, window) {
-    document.body.addEventListener("mouseup", () => {
-        let selectedText = window.getSelection();
-        let trimText = selectedText.replace(/[^\w\s]/gi, '');
-        let arrayOfWords = trimText.split(" ");
-        if (arrayOfWords.length > 0) {
-
-        }
-    });
-    document.body.addEventListener("click", () => {});
-
-    class Translateor {
+    class Translator {
         constructor() {
             this.SERVER_URL = "http://dictionary.skyeng.ru/api/v2/search-word-translation";
-            this.translator
-            this.imageBlock = null;
+            this.selectedItem = null;
+            this.translateContainer = document.createElement("div");
+            this.translateContainer.classList.add("translator");
+            this.translateContainer.addEventListener("mousedown", (event) => event.preventDefault());
+
+            this.imageBlock = document.createElement("img");
+            this.imageBlock.classList.add("translator__image");
+
+            this.meaningsList = document.createElement("ul");
+            this.meaningsList.classList.add("translator__meaning-list");
+            this.meaningsList.addEventListener("mouseover", this.onHoverMeaning.bind(this));
+
+            this.translateContainer.appendChild(this.imageBlock);
+            this.translateContainer.appendChild(this.meaningsList);
+            document.body.appendChild(this.translateContainer);
+        }
+
+        init(word, x, y) {
+            this.translateContainer.style.left = x;
+            this.translateContainer.style.top = y;
+            this.getTranslate(word)
+                .then((data) => {
+                    this.render(data);
+                    this.translateContainer.style.display = "block";
+                });
         }
 
         render(data) {
+            this.addMeaningsMarkup(data.meanings);
+        }
 
+        clear() {
+            this.translateContainer.style.display = "none";
+            this.imageBlock.src = "";
+            this.meaningsList.innerHTML = "";
+            this.selectedItem = null;
         }
 
         addMeaningsMarkup(meanings) {
-            let meaningsMarkup = "";
+            let firstItem = true;
             for (let meaning of meanings) {
-                meaningsMarkup += `
-                <span class="translater__meaning" data-image="${meaning.image_url}">
-                    ${meaning.name}
-                </span>
-            `;
+                let meaningElement = document.createElement("li");
+                meaningElement.classList.add("translator__meaning");
+                if (firstItem) {
+                    meaningElement.classList.add("translator__meaning--selected");
+                    this.imageBlock.src = `https:${meaning.image_url}`;
+                    this.selectedItem = meaningElement;
+                }
+                meaningElement.appendChild(document.createTextNode(meaning.translation));
+                meaningElement.setAttribute("data-image", `https:${meaning.image_url}`);
+                this.meaningsList.appendChild(meaningElement);
+                firstItem = false;
             }
-            let container = document.createElement("div");
-            container.classList.add("translater__meaning-list");
-            container.innerHTML = meaningsMarkup;
-            container.addEventListener("mouseover", this.onHoverMeaning.apply(this));
-            return container;
         }
 
         onHoverMeaning(event) {
-            event.target;
+            if (event.target.tagName !== "LI" || this.selectedItem === event.target) return;
+            let item = event.target;
+            this.selectedItem.classList.remove("translator__meaning--selected");
+            item.classList.add("translator__meaning--selected");
+            this.selectedItem = item;
+            this.imageBlock.src = item.dataset.image;
         }
 
         getTranslate(word) {
-            return fetch(`${this.SERVER_URL}?text=${word}`);
+            return fetch(`${this.SERVER_URL}?text=${word}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data.length > 0) {
+                        return data[0];
+                    }
+                    throw "No avialible translates";
+                });
         }
 
     }
+
+    const translator = new Translator();
+    document.body.addEventListener("mouseup", (event) => {
+        let selectedText = window.getSelection().toString();
+        if (!selectedText) {
+            return;
+        }
+        let trimText = selectedText.replace(/[^\w\s]/gi, '').trim();
+        if (!trimText) {
+            return;
+        }
+        let arrayOfWords = trimText.split(" ");
+        if (arrayOfWords.length <= 0) {
+            return;
+        }
+        translator.init(arrayOfWords[0], event.clientX, event.clientY);
+});
+
+document.body.addEventListener("mousedown", () => {
+    translator.clear();
+});
 
 })(document, window);
