de1973534282cc389b8ffff6951fae8a8589e1df
Konstantin Mamaev <meison@meison.net>
2017-04-05 02:06:20 +0300

    SQL tasks

---
+++ skyeng/sql/more 5 students.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(student_id), teacher.name
+FROM teacher_student
+LEFT JOIN teacher ON teacher.id = teacher_student.teacher_id
+GROUP BY teacher_student.teacher_id
+HAVING COUNT(student_id) > 5;
